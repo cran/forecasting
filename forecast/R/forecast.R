a8f4142627b791448edb1a4df55bf6747ce8785c
Rob J Hyndman <Rob.Hyndman@buseco.monash.edu.au>
2009-08-30 00:00:00 +0000

    version 1.26

--- forecast/R/forecast.R
+++ forecast/R/forecast.R
@@ -14,29 +14,95 @@ forecast.ts <- function(object, h=ifelse(frequency(object)>1, 2*frequency(object
         meanf(object,h=h,level=level,fan=fan,...)
 }
 
-print.forecast <- function(x ,...)
+as.data.frame.forecast <- function(x,...)
 {
-#    cat(paste("Call:\n",deparse(x$call),"\n\n"))
     nconf <- length(x$level)
-    out <- ts(matrix(x$mean,ncol=1))
+    out <- ts(matrix(x$mean, ncol = 1))
     attributes(out)$tsp <- attributes(x$mean)$tsp
     names <- c("Point Forecast")
-    if(!is.null(x$lower) & !is.null(x$upper) & !is.null(x$level))
-    {
+    if (!is.null(x$lower) & !is.null(x$upper) & !is.null(x$level)) {
         x$upper <- as.matrix(x$upper)
         x$lower <- as.matrix(x$lower)
-        for(i in 1:nconf)
-        {
-            out <- cbind(out,x$lower[,i],x$upper[,i])
-            names <- c(names,paste("Lo",x$level[i]),paste("Hi",x$level[i]))
+        for (i in 1:nconf) {
+            out <- cbind(out, x$lower[, i], x$upper[, i])
+            names <- c(names, paste("Lo", x$level[i]), paste("Hi", 
+                x$level[i]))
         }
     }
     colnames(out) <- names
     rownames(out) <- time(x$mean)
-    if(any(frequency(x$x) == c(4,12)))
-        print(out)
-    else
-        print(as.data.frame(out))
+    # Rest of function borrowed from print.ts(), but with header() omitted
+    x <- as.ts(out)
+    fr.x <- frequency(x)
+    calendar <- any(fr.x == c(4, 12)) && length(start(x)) ==  2L
+    Tsp <- tsp(x)
+    if (is.null(Tsp)) {
+        warning("series is corrupt, with no 'tsp' attribute")
+        print(unclass(x))
+        return(invisible(x))
+    }
+    nn <- 1 + round((Tsp[2L] - Tsp[1L]) * Tsp[3L])
+    if (NROW(x) != nn) {
+        warning(gettextf("series is corrupt: length %d with 'tsp' implying %d", 
+            NROW(x), nn), domain = NA, call. = FALSE)
+        calendar <- FALSE
+    }
+    if (NCOL(x) == 1) {
+        if (calendar) {
+            if (fr.x > 1) {
+                dn2 <- if (fr.x == 12) 
+                  month.abb
+                else if (fr.x == 4) {
+                  c("Qtr1", "Qtr2", "Qtr3", "Qtr4")
+                }
+                else paste("p", 1L:fr.x, sep = "")
+                if (NROW(x) <= fr.x && start(x)[1L] == end(x)[1L]) {
+                  dn1 <- start(x)[1L]
+                  dn2 <- dn2[1 + (start(x)[2L] - 2 + seq_along(x))%%fr.x]
+                  x <- matrix(format(x, ...), nrow = 1L, byrow = TRUE, 
+                    dimnames = list(dn1, dn2))
+                }
+                else {
+                  start.pad <- start(x)[2L] - 1
+                  end.pad <- fr.x - end(x)[2L]
+                  dn1 <- start(x)[1L]:end(x)[1L]
+                  x <- matrix(c(rep.int("", start.pad), format(x, 
+                    ...), rep.int("", end.pad)), ncol = fr.x, 
+                    byrow = TRUE, dimnames = list(dn1, dn2))
+                }
+            }
+            else {
+                tx <- time(x)
+                attributes(x) <- NULL
+                names(x) <- tx
+            }
+        }
+        else {
+            attr(x, "class") <- attr(x, "tsp") <- attr(x, "na.action") <- NULL
+        }
+    }
+    else {
+        if (calendar && fr.x > 1) {
+            tm <- time(x)
+            t2 <- 1 + round(fr.x * ((tm + 0.001)%%1))
+            p1 <- format(floor(zapsmall(tm)))
+            rownames(x) <- if (fr.x == 12) 
+                paste(month.abb[t2], p1, sep = " ")
+            else paste(p1, if (fr.x == 4) 
+                c("Q1", "Q2", "Q3", "Q4")[t2]
+            else format(t2), sep = " ")
+        }
+        else {
+            rownames(x) <- format(time(x))
+        }
+        attr(x, "class") <- attr(x, "tsp") <- attr(x, "na.action") <- NULL
+    }
+    return(as.data.frame(x))
+}
+
+print.forecast <- function(x ,...)
+{
+    print(as.data.frame(x))
 }
 
 
